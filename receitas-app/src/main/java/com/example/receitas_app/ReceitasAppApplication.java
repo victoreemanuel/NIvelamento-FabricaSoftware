989da50f59b722809d2faea3b64190a33e0c80f9
victoreemanuel <victoremanuelvm.31@hotmail.com>
2026-04-09 22:07:09 -0300

    Continuação avaliação - Fabrica de Software

---
+++ receitas-app/src/main/java/com/example/receitas_app/ReceitasAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.receitas_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceitasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceitasAppApplication.class, args);
+	}
+
+}
---
+++ receitas-app/src/test/java/com/example/receitas_app/ReceitasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.receitas_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
